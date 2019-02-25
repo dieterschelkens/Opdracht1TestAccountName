8977a4054fbaffe00ec08a7445830f249950bd7e
Ben Dierckx <BenDierckx1@hotmail.com>
2019-02-25 13:44:58 +0100

    First

---
+++ Opdracht1UnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opdracht1TestAccountName;
+
+namespace Opdracht1UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPersonGet2Names()
+        {
+            Person person = new Person();
+            string input = "Wim Beck";
+            string[] names = input.Split(' ');
+            person.firstName = names[0];
+            person.lastName = names[1];
+            Assert.AreEqual("Wim", person.firstName);
+            Assert.AreEqual("Beck", person.lastName);
+        }
+
+        [TestMethod]
+        public void TestPersonGenerateAccountIs6CharsLong()
+        {
+
+        }
+    }
+}
